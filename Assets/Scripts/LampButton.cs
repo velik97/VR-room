6dd8fc95c4072a93f330f661ead9804ca08230dd
Nikita Velikovskiy <velik.nikita@gmail.com>
2017-07-06 02:05:29 +0300

    minor commit

---
+++ Assets/Scripts/LampButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampButton : MonoBehaviour {
+
+	public Animator animator;
+	public AudioSource audioSource;
+
+	public GameObject onButton;
+	public GameObject offButton;
+
+	public float onPitch;
+	public float offPitch;
+
+	bool buttonIsOn;
+
+	void Start () {
+		buttonIsOn = true;
+	}
+
+	public void Set (GameObject presedButton) {
+		if (presedButton == onButton || presedButton ==  offButton) {
+			
+			bool _on = presedButton == onButton;
+
+			animator.SetBool ("On", _on);
+			Lamp.Instance.Set (_on);
+
+			if (buttonIsOn != _on) {
+				audioSource.pitch = _on ? onPitch : offPitch;
+				audioSource.Play ();
+			}
+
+			buttonIsOn = _on;
+		}
+
+	}
+}
